701de0e115ad0a1e735196f8ad2b9a45b5ab01ab
EJQuezada <equezada@msn.com>
2023-05-22 17:54:19 +0200

    Add code error handling logic in handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -90,6 +90,45 @@ module.exports.getCalendarEvents = event => {
   );
   const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
 
-  return new Promise( (resolve, reject) => { })
+  oAuth2Client.setCredentials({ access_token });
 
+  return new Promise( (resolve, reject) => { 
+
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject{error};
+        } else {
+          resolve{response};
+        }
+      }
+    );
+  
+  })
+  .then( results => {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ events: results.data.items })
+    };
+  })
+  .catch( error => {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(error),
+    };
+  });
+  
 }
