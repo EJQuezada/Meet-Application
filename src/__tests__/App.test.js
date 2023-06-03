ca7441a104173d19b4d12c778a3a40e21423a9d0
EJQuezada <equezada@msn.com>
2023-06-03 10:30:35 +0200

    added 'render CitySearch' test in App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,7 +5,11 @@ import EventList from '../EventList';
 
 describe('<App /> component', () => {
    test( 'render list of events', () => {
-    const AppWrapper = shallow(<App />);
-    expect(AppWrapper.find(EventList)).toHaveLength(1);
+      const AppWrapper = shallow(<App />);
+      expect(AppWrapper.find(EventList)).toHaveLength(1);
    }); 
+   test( 'render CitySearch', () => {
+      const AppWrapper = shallow(<App />);
+      expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+   });
 });
