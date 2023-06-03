220006d8fc98923fa95996f03d0406c0019f1683
EJQuezada <equezada@msn.com>
2023-06-03 13:29:00 +0200

    create api.js with basic functions

--- src/test/CitySearch.test.js
+++ src/test/CitySearch.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
+import { mockData } from '../mock-data';
 
 describe('<CitySearch /> component', () => {
     test('render text input', () => {
@@ -15,5 +16,15 @@ describe('<CitySearch /> component', () => {
         const CitySearchWrapper = shallow(<CitySearch />);
         const query = CitySearchWrapper.state('query');
         expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
-      });
+    });
+    test('render list of suggestions correctly', () => {
+        const locations = extractLocations(mockData);
+        const CitySearchWrapper = shallow(<CitySearch />);
+        CitySearchWrapper.setState({ suggestions: locations });
+        const suggestions = CitySearchWrapper.state('suggestions');
+        expect(CitySearchWrapper.find('.suggestion 1i')).toHaveLength(suggestions.length + 1);
+        for (let i = 0; i < suggestions.length; i += 1) {
+            expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
+        }
+    });
 });
