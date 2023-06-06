4a894bfe095d4db378ebb0a7c41a4781ace27a90
EJQuezada <equezada@msn.com>
2023-06-06 10:40:51 +0200

    update syntax of import functions

--- src/test/EventList.test.js
+++ src/test/EventList.test.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import EventList from '../EventList';
-import Event from './Event'; //in src/EventList.js
-import Event from '../Event'; //in src/test/EventList.test.js
-import { mockData } from '../mock-data';
+import React from "react";
+import { shallow } from "enzyme";
+import EventList from "../EventList";
+import Event from "./Event"; 
+import Event from "../Event"; 
+import { mockData } from "../mock-data";
 
-describe('<EventList /> component', () => {
-    test('render correct number of events', () => {
+describe("<EventList /> component", () => {
+    test("render correct number of events", () => {
         const EventListWrapper = shallow(<EventList events={mockData} />);
         expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
     });
