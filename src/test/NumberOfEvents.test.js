a76470b34e576fc32ac722acaf5394c4714af43e
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2023-06-08 15:03:29 +0200

    Update NumberOfEvents.test.js

--- src/test/NumberOfEvents.test.js
+++ src/test/NumberOfEvents.test.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import NumberOfEvents from '../NumberOfEvents';
+import React from "react";
+import { shallow } from "enzyme"';
+import NumberOfEvents from "../NumberOfEvents";
 
 describe("<NumberOfEvents/> component", () => {
     let NumberOfEventsWrapper;
@@ -8,14 +8,14 @@ describe("<NumberOfEvents/> component", () => {
         NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => {}} />);
     });
 
-    test("Default input is 24", () => {
-        expect(NumberOfEventsWrapper.state('query')).toBe(24);
+    test("Default input is 32", () => {
+        expect(NumberOfEventsWrapper.state("query")).toBe(32);
     });
 
     test("Value changes correctly", () => {
         NumberOfEventsWrapper.find(".numberOfEvents").simulate("change", {
-            target: { value: 18 },
+            target: { value: 20 },
         });
-        expect(NumberOfEventsWrapper.state("query")).toBe(18);
+        expect(NumberOfEventsWrapper.state("query")).toBe(20);
     });
 });
