2222ae03d9f6d105ee16b0dedd7690566284f2cf
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2023-06-12 15:31:14 +0200

    Update Event.test.js

--- src/test/Event.test.js
+++ src/test/Event.test.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Event from '../Event';
-import { mockData } from '../mock-data';
+import React from "react";
+import { shallow } from "enzyme";
+import Event from "../Event";
+import { mockData } from "../mock-data";
 
 describe("<Event /> component", () => {
-  let EventWrapper, event;
+  let EventWrapper;
+  const event = mockData[0];
   beforeAll(() => {
-    event = mockData[0];
     EventWrapper = shallow(<Event event={event} />);
   });
 
-  test("corresct details are rendered", () => {
-    const display = EventWrapper.find(".detailsButton");
-    display.simulate("click");
-    const summary = EventWrapper.find(".title");
-    const details = EventWrapper.find(".details li");
-    expect(summary.text()).toBe(`Summary: ${event.summary}`);
-    expect(details.at(0).text()).toBe(`Description: ${event.description}`);
-    expect(details.at(1).text()).toBe(`Location: ${event.location}`);
-    expect(details.at(2).text()).toBe(
-      `Start: ${new Date(event.start.dateTime).toISOString()}`
-    );
-    expect(details.at(3).text()).toBe(
-      `End: ${new Date(event.end.dateTime).toISOString()}`
-    );
+  test("renders the component", () => {
+    expect(EventWrapper).toBeDefined();
   });
 
-  test("toggle boolean", () => {
-    const display = EventWrapper.find(".detailsButton");
-    display.simulate("click");
-    expect(EventWrapper.state("hide")).toBe(true);
+  test("render event", () => {
+    expect(EventWrapper.find(".event")).toHaveLength(0);
   });
+
+  test("render event location", () => {
+    expect(EventWrapper.find(".event-location")).toHaveLength(0);
+  });
+
+  test("render event date", () => {
+      expect(EventWrapper.find(".event-start")).toHaveLength(0);
+  });
+
 });
