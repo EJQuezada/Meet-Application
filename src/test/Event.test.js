e9aa40f6318df2a4f074920f20888cb7f2272e94
EJQuezada <equezada@msn.com>
2023-06-12 14:07:27 +0200

    edit Event.test.js

--- src/test/Event.test.js
+++ src/test/Event.test.js
@@ -5,7 +5,7 @@ import { mockData } from "../mock-data";
 
 describe("<Event /> component", () => {
   let EventWrapper;
-  const event = mockData[1];
+  const event = mockData[0];
   beforeAll(() => {
     EventWrapper = shallow(<Event event={event} />);
   });
