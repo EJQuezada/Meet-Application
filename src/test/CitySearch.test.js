fc0c89eb8639766e623b6a300e3a43a6167893dd
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2023-06-12 22:29:33 +0200

    Update CitySearch.test.js
    change .suggestion to .suggestions

--- src/test/CitySearch.test.js
+++ src/test/CitySearch.test.js
@@ -28,7 +28,7 @@ describe('<CitySearch /> component', () => {
         const CitySearchWrapper = shallow(<CitySearch />);
         CitySearchWrapper.setState({ suggestions: locations });
         const suggestions = CitySearchWrapper.state('suggestions');
-        expect(CitySearchWrapper.find('.suggestion li')).toHaveLength(suggestions.length + 1);
+        expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
         for (let i = 0; i < suggestions.length; i += 1) {
             expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
         }
