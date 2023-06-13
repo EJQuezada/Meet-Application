f1abe0df70edbfacd9288ddfa6d0a0c6de0e41bc
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2023-06-13 12:01:43 +0200

    Create App.test.js and add basic components for testing

---
+++ src/test/App.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App";
+import EventList from "../EventList";
+import CitySearch from "../CitySearch";
+import NumberOfEvents from "../NumberOfEvents";
+
+describe("<App /> component", () => { // describe() function groups tests together
+    let AppWrapper;
+    beforeAll(() => {
+        AppWrapper = shallow(<App />); // shallow rendering
+    });
+
+    test("render list of events", () => {
+        expect(AppWrapper.find(EventList)).toHaveLength(1); // check if EventList component is rendered
+    });
+
+    test("render CitySearch", () => {
+        expect(AppWrapper.find(CitySearch)).toHaveLength(1); // check if CitySearch component is rendered
+    });
+
+    test("render NumberOfEvents", () => {
+        expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1); // check if NumberOfEvents component is rendered
+    });
+
+
+});
