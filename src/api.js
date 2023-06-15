842982a70de4d5304f52adf3e8cc10d4c54d36bd
EJQuezada <equezada@msn.com>
2023-06-15 10:40:22 +0200

    update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import { mockData } from "./mock-data";
-import { extractLocations, getEvents } from "../api";
+
 export const getEvents = async () => {
     return mockData;
 };
+
 export const extractLocations = (events) => {
     var extractLocations = events.map((event) => event.location);
     var locations = [...new Set(extractLocations)];
