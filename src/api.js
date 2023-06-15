a0efbf432e0794ad15850ac537a31af01d978be5
EJQuezada <equezada@msn.com>
2023-06-15 21:28:27 +0200

    update api.js functions

--- src/api.js
+++ src/api.js
@@ -2,7 +2,27 @@ import { mockData } from "./mock-data";
 import axios from "axios";
 
 export const getEvents = async () => {
-    return mockData;
+    NProgress.start();
+
+    if (window.location.href.startsWith('http://localhost')) {
+        NProgress.done();
+        return mockData;
+    }
+
+    const token = await getAccessToken();
+
+    if (token) {
+        removeQuery();
+        const url = "YOUR_GET_EVENTS_API_ENDPOINT" + "/" + token;
+        const result = await axios.get(url);
+        if (result.data) {
+            var locations = extractLocations(result.data.events);
+            localStorage.setItem('lastEvents', JSON.stringify(result.data));
+            localStorage.setItem('locations', JSON.stringify(locations));
+        }
+        NProgress.done();
+        return result.data.events;
+    }
 };
 
 export const extractLocations = (events) => {
@@ -14,18 +34,30 @@ export const extractLocations = (events) => {
 export const getAccessToken = async () => {
     const accessToken = localStorage.getItem('access_token');
     const tokenCheck = accessToken && (await checkToken(accessToken));
-        if (!accessToken || tokenCheck.error) {
-            await localStorage.removeItem('access_token');
-            const searchParams = new URLSearchParams(window.location.search);
-            const code = await searchParams.get('code');
-            if (!code) {
-                const results = await axios.get(
-                    "YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
-                );
-                const { authUrl } = results.data;
-                return (window.location.href = authUrl);
-            }
-            return code && getToken(code);
+    
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem('access_token');
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get('code');
+        if (!code) {
+            const results = await axios.get(
+                "YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
+            );
+            const { authUrl } = results.data;
+            return (window.location.href = authUrl);
         }
-        return accessToken;
-}
+        return code && getToken(code);
+    }
+    return accessToken;
+};
+
+export const checkToken = async (accessToken) => {
+    const result = await fetch(
+        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    )
+        .then((res) => res.json())
+        .catch((error) => error.json());
+
+    return result;
+};
+
