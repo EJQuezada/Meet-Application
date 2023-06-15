78f756419746b54b455f1f7c7b5e7b2a6d6b2989
EJQuezada <equezada@msn.com>
2023-06-15 11:55:28 +0200

    fix parsing error in EventList.js file

--- src/EventList.js
+++ src/EventList.js
@@ -4,25 +4,25 @@ import CitySearch from "./CitySearch";
 
 class EventList extends Component {
     render() {
-        return (
-            <div className="App">
-                <CitySearch />
-                <EventList events={this.state.events} />
-            </div>
-        ); 
+    //    return (
+    //        <div className="App">
+    //            <CitySearch />
+    //            <EventList events={this.state.events} />
+    //        </div>
+    //    ); 
     //{
-        //const { events } = this.props;
-        //return (
-        //    <ul className='EventList'>
-        //        {this.props.events.map(event) => ( 
-        //            <li key={event.id}>
-        //                <Event event={event} />
-        //            </li>
-        //        )}
-        //    </ul>
-        //);
-    //}
-    };
-}
+        const { events } = this.props;
+        return (
+            <ul className='EventList'>
+                {events.map((event) => ( 
+                    <li key={event.id}>
+                        <Event event={event} />
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+};
+
 
 export default EventList;
