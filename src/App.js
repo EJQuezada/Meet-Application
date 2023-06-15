7c61bceaa4f6d0f938ee7e2037963ffb7493bc3a
EJQuezada <equezada@msn.com>
2023-06-15 10:53:55 +0200

    edit App.js folder

--- src/App.js
+++ src/App.js
@@ -12,29 +12,29 @@ class App extends Component {
     locations: [],
   }
 
-componentDidMount() {
-  this.mounted = true;
-  getEvents().then((events) => {
-    if (this.mounted) {
-      this.setState({ events, locations: extractLocations(events) });
-    }
-  });
-}
-
-componentWillUnmount(){
-  this.mounted = false;
-}
-
-updateEvents = (location) => {
-  getEvents().then((events) => {
-    const locationEvents = (location === "all") ?
-      events:
-      events.filter((event) => event.location === location);
-    this.setState({
-      events: locationEvents
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
     });
-  });
-}
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === "all") ?
+        events:
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
 
   render() {
     return (
