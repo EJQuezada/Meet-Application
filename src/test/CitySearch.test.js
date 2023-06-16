f1547d0df99a5d07d9740607332051f1b8e86c59
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2023-06-16 13:19:10 +0200

    Update CitySearch.test.js file

--- src/test/CitySearch.test.js
+++ src/test/CitySearch.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import CitySearch from '../CitySearch';
-import { mockData } from '../mock-data';
-import { extractLocations } from '../api';
+import React from "react";
+import { shallow } from "enzyme";
+import CitySearch from "../CitySearch";
+import { mockData } from "../mock-data";
+import { extractLocations } from "../api";
 
 describe('<CitySearch /> component', () => {
     let locations, CitySearchWrapper;
     beforeAll(() => {
         locations = extractLocations(mockData);
-        CitySearchWrapper = shallow(<CitySearch locations={locations} />);
+        CitySearchWrapper = shallow(<CitySearch locations={locations} updateEvents={() => {}} />);
     });
     test('render text input', () => {
         const CitySearchWrapper = shallow(<CitySearch />);
@@ -51,5 +51,19 @@ describe('<CitySearch /> component', () => {
         const suggestions = CitySearchWrapper.state("suggestions");
         CitySearchWrapper.find(".suggestions li").at(0).simulate("click");
         expect(CitySearchWrapper.state('query')).toBe(suggestions[0]);
+    });
+    test('selecting CitySearch input reveals the suggestions list', () => {
+        CitySearchWrapper.find(".city").simulate("focus");
+        expect(CitySearchWrapper.state("showSuggestions")).toBe(true);
+        expect(CitySearchWrapper.find(".suggestions").prop("style")).not.toEqual({ display: "none" });
+    });
+    test('selecting a suggestion should hide the suggestions list', () => {
+        CitySearchWrapper.setState({
+            query: "Berlin",
+            showSuggestions: undefined
+        });
+        CitySearchWrapper.find(".suggestions li").at(0).simulate("click");
+        expect(CitySearchWrapper.state("showSuggestions")).toBe(false);
+        expect(CitySearchWrapper.find(".suggestions").prop("style")).toEqual({ display: "none" });
     });
 });
