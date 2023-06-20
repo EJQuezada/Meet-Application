554ec28de846232c49273533c8a658aadd8f317c
EJQuezada <equezada@msn.com>
2023-06-20 13:54:43 +0200

    update EndToEnd.test.js file

--- src/test/EndToEnd.test.js
+++ src/test/EndToEnd.test.js
@@ -15,31 +15,29 @@ describe('show/hide an event details', () => {
         browser.close();
     });
 
-    test()
-});
-
     test('An event element is collapsed by default', async () => {
-        const browser = await puppeteer.launch();
+        //const browser = await puppeteer.launch();
 
-        const page = await browser.newPage();
-        await page.goto('http://localhost:3000/');
+        //const page = await browser.newPage();
+        //await page.goto('http://localhost:3000/');
 
-        await page.waitForSelector('.event');
+        //await page.waitForSelector('.event');
 
         const eventDetails = await page.$('.event.event__Details');
         expect(eventDetails).toBeNull();
-        browser.close();
+        //browser.close();
     });
 
     test('User can expand an event to see its details', async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('http://localhost:3000/');
+        //const browser = await puppeteer.launch();
+        //const page = await browser.newPage();
+        //await page.goto('http://localhost:3000/');
 
-        await page.waitForSelector('.event');
+        //await page.waitForSelector('.event');
         await page.click('.event .details-btn');
 
         const eventDetails = await page.$('.event.event__Details');
         expect(eventDetails).toBeDefined();
-        browser.close();
+        //browser.close();
     });
+});
