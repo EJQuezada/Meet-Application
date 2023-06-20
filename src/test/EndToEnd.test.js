43695b2327e2afac5840186ed7efc2f48296811a
EJQuezada <equezada@msn.com>
2023-06-20 14:12:33 +0200

    edit setTimeout function to greater time

--- src/test/EndToEnd.test.js
+++ src/test/EndToEnd.test.js
@@ -3,10 +3,10 @@ import puppeteer from 'puppeteer';
 describe('show/hide an event details', () => {
     let browser;
     let page;
-    //jest.setTimeout(30000);
+    jest.setTimeout(30000);
 
     beforeAll(async () => {
-        jest.setTimeout(30000);
+        //jest.setTimeout(30000);
         browser = await puppeteer.launch();
         page = await browser.newPage();
         await page.goto('http://localhost:3000/');
