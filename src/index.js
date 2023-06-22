cd0b56fe300dfb322967ff221ca6e8bfb3e8ffab
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2023-06-22 16:02:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import * as atatus from 'atatus-spa';
+atatus.config('6a431a8aae32419fbb27cfa92d319c57').install();
 
 const root = document.getElementById('root');
 render(<App />, root);
@@ -18,3 +20,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//atatus.notify(new Error('Test Atatus Setup'));
