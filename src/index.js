902cd234201aab024db8c657221ea4508d849900
EJQuezada <equezada@msn.com>
2023-06-28 14:47:25 +0200

    change atatus-spa to atatus-js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceworker from "./service-worker";
-import * as atatus from "atatus-spa";
+import * as atatus from "atatus-js";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 //import reportWebVitals from "./reportWebVitals";
 
