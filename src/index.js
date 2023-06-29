6ac0110a466c9552f5a7a3680ac5ff64fa001814
EJQuezada <equezada@msn.com>
2023-06-29 14:25:08 +0200

    edit syntax serviceWorkerRegistration in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
@@ -8,18 +8,17 @@ import * as atatus from "atatus-spa";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 //import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = document.getElementById("root");
+render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>, 
-    document.getElementById("root"));
-const root = document.getElementById('root');
-render(<App />, root);
+    </React.StrictMode>, root); 
+    
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
