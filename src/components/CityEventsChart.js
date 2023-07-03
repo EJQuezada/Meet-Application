6f85b1416a22b8770efc59b618202f508190b574
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2023-07-04 00:43:21 +0200

    Add Components and Features files to repository

---
+++ src/components/CityEventsChart.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import {
+    ScatterChart,
+    Scatter,
+    XAxis, YAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer
+} from 'recharts';
+
+const CityEventsChart = ({ allLocations, events }) => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        setData(getData());
+    }, [`${data}`]);
+
+    const getData = () => {
+        const data = allLocations.map((location) => {
+            const count = events.filter((event) => event.location === location).length
+            const city = location.split((/, | - /))[0]
+            return { city, count };    
+        })
+        return data;
+    };
+
+    return (
+        <ResponsiveContainer width="99%" height={400}>
+            <PieChart>
+            <Pie
+                data={data}
+                dataKey="value"
+                fill="#8884d8"
+                labelLine={false}
+                label
+                outerRadius={130}           
+            />
+            </PieChart>
+        </ResponsiveContainer>
+    );
+}
+
+export default CityEventsChart;
