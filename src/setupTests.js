0e232bd936f8bf89616aa29a5287f71e978ed0b0
EJQuezada <equezada@msn.com>
2023-07-03 22:27:51 +0200

    update setupTests.js file

--- src/setupTests.js
+++ src/setupTests.js
@@ -7,3 +7,15 @@ import Enzyme from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+    //@ts-ignore
+    delete window.ResizeObserver;
+    window.ResizeObserver = jest.fn().mockImplementation(() => ({
+        observe: jest.fn(),
+        unobserve: jest.fn(),
+        disconnect: jest.fn(),
+    }));
+});
