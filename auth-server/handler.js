ace35cc8b365795744e1559b235053a7ec755d49
EJQuezada <equezada@msn.com>
2023-07-06 22:18:09 +0200

    fix typo in handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,7 +19,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://ejquezada.github.io/Meet/"],
+  redirect_uris: ["https://ejquezada.github.io/Meet-Application/"],
   javascript_origins: [
     "https://ejquezada.github.io", 
     "http://localhost:3000"
