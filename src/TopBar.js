a28c5ea9593edfc34abb3734ec73579e5825928b
EJQuezada <115997231+EJQuezada@users.noreply.github.com>
2023-07-12 01:02:06 +0200

    Add files to project
    Added ThemeChanger.js, TopBar.js, WelcomeScreen.css, and WelcomeScreen.jsx files to repository

---
+++ src/TopBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ThemeChanger from "./ThemeChanger";
+
+class TopBar extends Component {
+    render() {
+        return (
+            <div className="TopBar">
+                <h1>NOFOMO</h1>
+                <h3 className="subtitle text-white">No Fear of Missing Out!</h3>
+                <ThemeChanger />
+            </div>
+        );
+    }
+}
+
+export default TopBar;
