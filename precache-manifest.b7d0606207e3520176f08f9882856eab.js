270512481897a5baa40ec15473a70a809ec267a9
EJQuezada <equezada@msn.com>
2023-07-12 13:32:28 +0200

    Updates

---
+++ precache-manifest.b7d0606207e3520176f08f9882856eab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ee1ce3590f856ce4e0e",
+    "url": "/Meet-Application/static/css/main.6bfa953e.chunk.css"
+  },
+  {
+    "revision": "7ee1ce3590f856ce4e0e",
+    "url": "/Meet-Application/static/js/main.f168dc73.chunk.js"
+  },
+  {
+    "revision": "b96819057fbc2b670748",
+    "url": "/Meet-Application/static/js/runtime~main.33fcc0e1.js"
+  },
+  {
+    "revision": "7a85e2a101a18c4a7d8d",
+    "url": "/Meet-Application/static/js/2.1edf1bf5.chunk.js"
+  },
+  {
+    "revision": "e5a570405181041622b1",
+    "url": "/Meet-Application/static/js/3.f470ba38.chunk.js"
+  },
+  {
+    "revision": "8e26911b3a036e6227d7ba47bab7876e",
+    "url": "/Meet-Application/index.html"
+  }
+];
